2c4783e878eadead88b45dd2d36fc1acdf5698d5
SimpllleUser <raybek11@gmail.com>
2020-11-25 12:52:57 +0300

    Get data by list coins

---
+++ src/components/CoinsTabel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="coins-tabel">
+<!--   <h1>Coin tabel</h1>-->
+    <div>{{itemsCoin}}</div>
+    <b-table striped hover  :fields="fields" :items="itemsCoin">
+
+    </b-table>
+  </div>
+</template>
+
+<script>
+// import axios from "axios"
+export default {
+  name: "coins-tabel",
+  props:['coinsList'],
+  data(){
+    return{
+      itemsCoin:[]
+    }
+  },
+  methods: {
+    formatDataToTabel(){
+      this.itemsCoin =  this.coinsList.map((item, index) => ({id: index + 1, Name: item.FullName, label: item.Name, }))
+    }
+  },
+  created () {
+    this.formatDataToTabel()
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,57 @@
 <template>
   <div class="home">
-    {{allData.length > 10 && JSON.parse(allData)}}
+    {{ allData.length > 10 && JSON.parse(allData) }}
+    <coins-tabel :coinsList="coinsList"/>
   </div>
 </template>
 
 <script>
 import axios from "axios"
 import HelloWorld from "@/components/HelloWorld.vue";
+import CoinsTabel from "@/components/CoinsTabel";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    CoinsTabel
   },
-  data(){
-    return{
-      KEY:'50e081a896903ee4c80ba4e38bcc4073b4aa22473f8d460a3d05039ee37310c8',
-      API_URL:"https://min-api.cryptocompare.com",
+  data() {
+    return {
+      KEY: '50e081a896903ee4c80ba4e38bcc4073b4aa22473f8d460a3d05039ee37310c8',
+      API_URL: "https://min-api.cryptocompare.com",
       // API_URL:"https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=",
-      coinList:[],
-      allData:{}
+      coinsList: [],
+      allData: {},
+      paramsCoinsWS: []
     }
   },
   methods: {
-    async getListCoins(){
+    async getListCoins() {
       const limit = "20"
-
       try {
-        const res = await axios.get(this.API_URL + `/data/top/mktcapfull?limit=${limit}&tsym=USD`,{})
-        this.coinList = res.data.Data.map(coin => coin.CoinInfo)
-      }catch (e) {
-        console.log('err /n get coinList', e)
+        const res = await axios.get(this.API_URL + `/data/top/mktcapfull?limit=${limit}&tsym=USD`, {})
+        this.coinsList = res.data.Data.map(coin => coin.CoinInfo)
+        const paramsName = this.coinsList.map(coin => coin.Name)
+        this.paramsCoinsWS = paramsName.map(param => ({
+          name: param,
+          paramItem: `5~CCCAGG~${param}~USD`
+        }))
+      } catch (err) {
+        console.log('err /n get coinsList', err)
       }
-      console.log('coinList',this.coinList)
+      console.log('coinsList', this.coinsList)
     },
     initialData() {
       const ccStreamer = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + this.KEY);
-      // ccStreamer.close()
+      const v = this
       ccStreamer.onopen = function onStreamOpen() {
         const subRequest = {
           "action": "SubAdd",
-          "subs": ["5~CCCAGG~BTC~USD", "0~CCCAGG~ETH~USD",]
+          "subs": v.paramsCoinsWS.map(param => param.paramItem)
         };
         ccStreamer.send(JSON.stringify(subRequest));
       }
-      const v = this
       ccStreamer.onmessage = function onStreamMessage(message) {
         var message = event.data;
         v.allData = event.data
@@ -52,7 +59,7 @@ export default {
       }
     },
   },
-  async created () {
+  async created() {
     console.log("Start data");
     this.initialData();
     await this.getListCoins()
