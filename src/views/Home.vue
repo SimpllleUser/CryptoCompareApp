52ffedc8f93f36ba940b074be9b70496b30c8ca9
SimpllleUser <raybek11@gmail.com>
2020-11-25 12:18:59 +0300

    Init coinList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="home">
-    {{allData}}
-<!--    <img alt="Vue logo" src="../assets/logo.png" />-->
-<!--    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    {{allData.length > 10 && JSON.parse(allData)}}
   </div>
 </template>
 
 <script>
+import axios from "axios"
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
@@ -16,23 +15,32 @@ export default {
   },
   data(){
     return{
-      coinList:[
-        {
-          //cutName:"",
-          // name:""
-        }
-      ],
+      KEY:'50e081a896903ee4c80ba4e38bcc4073b4aa22473f8d460a3d05039ee37310c8',
+      API_URL:"https://min-api.cryptocompare.com",
+      // API_URL:"https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=",
+      coinList:[],
       allData:{}
     }
   },
   methods: {
+    async getListCoins(){
+      const limit = "20"
+
+      try {
+        const res = await axios.get(this.API_URL + `/data/top/mktcapfull?limit=${limit}&tsym=USD`,{})
+        this.coinList = res.data.Data.map(coin => coin.CoinInfo)
+      }catch (e) {
+        console.log('err /n get coinList', e)
+      }
+      console.log('coinList',this.coinList)
+    },
     initialData() {
-      const apiKey = "aaa832ab7795e6f6fec2a270dfea998f1a3f701662fba05406651069b6dc9cee";
-      const ccStreamer = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + apiKey);
+      const ccStreamer = new WebSocket('wss://streamer.cryptocompare.com/v2?api_key=' + this.KEY);
+      // ccStreamer.close()
       ccStreamer.onopen = function onStreamOpen() {
         const subRequest = {
           "action": "SubAdd",
-          "subs": ["5~CCCAGG~BTC~USD", "0~Coinbase~ETH~USD", "2~Binance~BTC~USDT"]
+          "subs": ["5~CCCAGG~BTC~USD", "0~CCCAGG~ETH~USD",]
         };
         ccStreamer.send(JSON.stringify(subRequest));
       }
@@ -40,13 +48,14 @@ export default {
       ccStreamer.onmessage = function onStreamMessage(message) {
         var message = event.data;
         v.allData = event.data
-        console.log("Received from Cryptocompare: " + message);
+        console.log(message);
       }
     },
   },
-  created() {
+  async created () {
     console.log("Start data");
     this.initialData();
+    await this.getListCoins()
   },
 };
 </script>
